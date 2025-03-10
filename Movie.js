e48c3ee99c898abc461010c4f5b52c66711bfd93
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-10 18:10:38 +0100

    prøver at lave javascript til addmovie funktionaliteten

--- Movie.js
+++ Movie.js
@@ -1,6 +1,8 @@
 const url = "http://localhost:8080";
 //const url = "http://rigtig-url";
 
+
+
 fetch(`${url}/movie/all`)
     .then(response => {
         if (!response.ok) {
@@ -38,5 +40,38 @@ function displayMovieText(movies) {
 
         div.append(movieinfo);
     })
+}
 
+
+
+function addMovie(movie) {
+    const div = document.querySelector("#movie-list");
+
+    const addMovie = document.createElement("div");
+    movieinfo.classList.add("addMovie");
+
+    addMovie.innerHTML = `
+    <h2>Add Movie</h2>
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title" value="${movie.title || ''}"><br>
+
+    <label for="releaseYear">Udgivelsesår:</label>
+    <input type="number" id="releaseYear" name="releaseYear" value="${movie.releaseYear || ''}"><br>
+
+    <label for="duration">Varighed:</label>
+    <input type="text" id="duration" name="duration" value="${movie.duration || ''}"><br>
+
+    <label for="ageRestriction">Aldersgrænse:</label>
+    <input type="text" id="ageRestriction" name="ageRestriction" value="${movie.ageRestriction || ''}"><br>
+
+    <label for="description">Beskrivelse:</label>
+    <textarea id="description" name="description">${movie.description || ''}</textarea><br>
+
+    <label for="genre">Genre:</label>
+    <input type="text" id="genre" name="genre" value="${movie.genre || ''}"><br>
+
+    <label for="director">Diraktør:</label>
+    <input type="text" id="director" name="director" value="${movie.director || ''}"><br>
+    `;
+    div.append(movieinfo);
 }
--- Showing.js
+++ Showing.js
@@ -1,5 +1,5 @@
-const url = "http://localhost:8080";
-//const url = "http://rigtig-url";
+//const url = "http://localhost:8080";
+const url = "http://52.169.6.120";
 
 fetch(`${url}/showing/all`)
     .then(response => {
