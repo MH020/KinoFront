93bfd171a39a0a9e32110feef18096f50439eeba
MH020 <166133291+MH020@users.noreply.github.com>
2025-03-11 04:29:16 +0100

    2 tilføjet addmovie metode

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log('Happy developing ✨')
+
+function addMovie(movie) {
+    const div = document.querySelector("#movie-list");
+
+    const addMovie = document.createElement("div");
+    movieinfo.classList.add("addMovie");
+
+    addMovie.innerHTML = `
+    <h2>Add Movie</h2>
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title" value="${movie.title || ''}"><br>
+
+    <label for="releaseYear">Udgivelsesår:</label>
+    <input type="number" id="releaseYear" name="releaseYear" value="${movie.releaseYear || ''}"><br>
+
+    <label for="duration">Varighed:</label>
+    <input type="text" id="duration" name="duration" value="${movie.duration || ''}"><br>
+
+    <label for="ageRestriction">Aldersgrænse:</label>
+    <input type="text" id="ageRestriction" name="ageRestriction" value="${movie.ageRestriction || ''}"><br>
+
+    <label for="description">Beskrivelse:</label>
+    <textarea id="description" name="description">${movie.description || ''}</textarea><br>
+
+    <label for="genre">Genre:</label>
+    <input type="text" id="genre" name="genre" value="${movie.genre || ''}"><br>
+
+    <label for="director">Diraktør:</label>
+    <input type="text" id="director" name="director" value="${movie.director || ''}"><br>
+    `;
+    div.append(movieinfo);
+}
