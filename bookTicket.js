7b2af05dfe6517f87c7db8c3f325034143823f9a
jazzyzoney <jaqtash22@gmail.com>
2025-03-12 10:19:31 +0100

    noget?

--- bookTicket.js
+++ bookTicket.js
@@ -1,16 +1,43 @@
-async function fetchBookingData() {
-    try {
-        const response = await fetch("https://localhost"); // replace
-        const data = await response.json();
+const url = "http://localhost:8080";
+//const url = "http://rigtig-url";
 
-        document.getElementById("movie").value = data.movie;
-        document.getElementById("time").value = data.time;
-        document.getElementById("duration").value = data.duration;
-        document.getElementById("theatre").value = data.theatre;
-    } catch (error) {
-        console.error("Error fetching booking data:", error);
+fetch(`${url}/ticket/tickets`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("FEJL NUMBNUTS");
+        }
+        return response.json();
+    })
+    .then(tickets => {
+        console.log(tickets);
+        displayTicket(tickets);
+    })
+    .catch(err => {
+        console.error("error", err);
+    });
+
+function displayTicket(tickets) {
+    if (!tickets) {
+        console.warn("no ticks found.");
+        return;
     }
+
+    const div = document.querySelector("#showing-list");
+
+    tickets.forEach(ticket => {
+        const ticketInfo = document.createElement("div");
+        ticketInfo.classList.add("ticketInfo");
+
+        ticketInfo.innerHTML = `
+            <h2>Movie: ${ticket.movie.title}</h2>
+            <p><strong>Date:</strong> ${ticket.date}</p>
+            <p><strong>Time:</strong> ${ticket.time}</p>
+            <p><strong>Duration:</strong> ${ticket.movie.duration}</p>
+            <p><strong>Theatre:</strong> #${ticket.theatre.id}</p>
+            <p><strong>Phone Number:</strong> #${ticket.phoneNumber}</p>
+            <hr>
+        `;
+
+        div.append(ticketInfo);
+    })
 }
-
-fetchBookingData();
-
